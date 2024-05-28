cb9f62a9c752e71e830245afaf33a8d86de599c7
Alex_qp <alex-schmid11@web.de>
2024-05-28 21:27:37 +0200

    MC 1.20.6, Java 21, License

--- redye_core/src/main/java/com/github/alexqp/redye/main/RedyeMaterial.java
+++ redye_core/src/main/java/com/github/alexqp/redye/main/RedyeMaterial.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018-2023 Alexander Schmid
+ * Copyright (C) 2018-2024 Alexander Schmid
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
